c5c834b05f4283f4fe92c53dba8f51844b275988
olilewis1 <olilewis1@hotmail.co.uk>
2024-01-30 11:28:34 +0000

    Updated server

--- airsenal/scripts_to_work/server.js
+++ airsenal/scripts_to_work/server.js
@@ -146,5 +146,5 @@ app.post('/return-optimization', (req, res) => {
       });
     });
 app.listen(PORT, () => { 
-  console.log('Server is running on port 3000');
+  console.log(`Server is running on port ${PORT}`);
 });
