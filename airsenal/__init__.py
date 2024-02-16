6842ff6095fad0c20d7e6fe7fea12cfcbc2eacdd
olilewis1 <olilewis1@hotmail.co.uk>
2024-02-16 22:03:49 +0000

    updated procfile

--- airsenal/__init__.py
+++ airsenal/__init__.py
@@ -2,8 +2,20 @@
 ___init__.py for airsenal
 """
 
-from importlib.metadata import version
+import os
+import toml
 
-# AIrsenal package version.
-__version__ = version('airsenal')
+# Get the directory path of the current script
+current_dir = os.path.dirname(os.path.abspath(__file__))
 
+# Construct the path to pyproject.toml by navigating one directory up
+pyproject_path = os.path.join(current_dir, '..', 'pyproject.toml')
+
+# Load pyproject.toml file
+with open(pyproject_path, 'r') as file:
+    pyproject = toml.load(file)
+
+# Access the version from the tool.poetry section
+version = pyproject['tool']['poetry']['version']
+
+print("AIrsenal project version:", version)
